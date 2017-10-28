a952d34135134246bff9bc1d8ca615e76cb1c010
hkedia321 <harshit.kedia2015@vit.ac.in>
2017-10-28 14:14:40 +0530

    show loggedIn name

--- js/script.js
+++ js/script.js
@@ -11,21 +11,28 @@ function Register(regno,name) {
         Materialize.toast('All fields are required!',3000);
         return;
     }
+    localStorage.setItem("regno",regno);
+    localStorage.setItem("name",name);
     database.ref(`users/${regno}`).set({
         name: name
     }).then(function () {
+        fetchQuestion();
         Materialize.toast('Registered successfully!',3000);
         $("#loginBlock").fadeOut();
         $("#logoutBlock").fadeIn();
-        var current = database.ref(`currentQuestion/value`);
-        current.on('value', function(snapshot) {
-            $("#questionsBlock").fadeIn();
-            qn=snapshot.val();
-            $('#ans').val('');
-            $('#qn').html(`${qn}`);
-            Materialize.toast('New question!',3000);
-            console.log('current question is - ',snapshot.val());
-        });
+        $("#loggedRegn").text(g_regno);
+        $("#loggedName").text(g_name);
+    });
+}
+function fetchQuestion(){
+    var current = database.ref(`currentQuestion/value`);
+    current.on('value', function(snapshot) {
+        $("#questionsBlock").fadeIn();
+        qn=snapshot.val();
+        $('#ans').val('');
+        $('#qn').html(`${qn}`);
+        Materialize.toast('New question!',3000);
+        console.log('current question is - ',snapshot.val());
     });
 }
 function Submit(regno,name,val){
@@ -47,6 +54,16 @@ function Submit(regno,name,val){
     });
 }
 $(function () {
+    if(localStorage.getItem("regno")){
+        $("#loginBlock").hide();
+        $("#logoutBlock").show();
+        $("#questionsBlock").show();
+        g_reg=localStorage.getItem("regno");
+        g_name=localStorage.getItem("name");
+        $("#loggedRegno").text(g_reg);
+        $("#loggedName").text(g_name);
+        fetchQuestion();
+    }
     $( "#registerForm").submit(function( event ) {
         Register($("#regno").val(),$('#name').val());
         event.preventDefault();
@@ -54,5 +71,10 @@ $(function () {
     $('#sub').click(function () {
         if(g_name&&g_reg&&qn)
         Submit(g_reg,g_name,$('#ans').val());
+    });
+    $("#logout").click(function(){
+        localStorage.removeItem("regno");
+        localStorage.removeItem("name");
+        document.location.reload();
     });
 });
